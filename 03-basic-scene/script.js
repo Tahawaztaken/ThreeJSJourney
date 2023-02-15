55952384f9d431c6a8bb885fc2d61b04965037bc
Syed Taha Faisal <faisasye@sheridancollege.ca>
2023-02-14 21:51:54 -0500

    Practiced a bit more.

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -29,3 +29,42 @@ const renderer = new THREE.WebGLRenderer({              //Remember the renderer 
 //We need to resize the renderer to match the camera
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera) // renderer needs scene and camera to init
+
+
+//Some practice
+
+// const scene = new THREE.Scene()
+
+// const geometry = new THREE.CylinderGeometry(2, 5, 10, 16)
+// const material = new THREE.MeshBasicMaterial({color: 'red'})
+// const cylinder = new THREE.Mesh(geometry, material)
+// scene.add(cylinder)
+
+// const geometry_capsule = new THREE.CapsuleGeometry(1, 1, 4, 8)
+// const material_capsule = new THREE.MeshBasicMaterial({color: 'green'})
+// const capsule = new THREE.Mesh(geometry_capsule, material_capsule)
+// scene.add(capsule)
+// capsule.position.x = 0
+// capsule.position.y = 10
+// capsule.position.z = 10
+
+
+
+// const sizes = {
+//     width: 1000,
+//     height: 800
+// }
+
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// scene.add(camera)
+// camera.position.z = 20
+// camera.position.x = 0
+// camera.position.y = 8
+
+// const canvas = document.querySelector('.webgl')
+// const renderer = new THREE.WebGLRenderer({
+//     canvas: canvas
+// })
+// renderer.setSize(sizes.width, sizes.height)
+
+// renderer.render(scene, camera)
