8c5a7ac39aa0ba3a2a6a7cab8a58754757c0542f
Syed Taha Faisal <faisasye@sheridancollege.ca>
2023-08-27 16:58:35 -0400

    Earth model loaded

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,21 +1,47 @@
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.CylinderGeometry(2, 5, 10, 16)
-const material = new THREE.MeshBasicMaterial({color: 'red'})
-const cylinder = new THREE.Mesh(geometry, material)
-scene.add(cylinder)
+// Instantiate a loader
+const loader = new GLTFLoader();
 
-const geometry_capsule = new THREE.CapsuleGeometry(1, 1, 4, 8)
-const material_capsule = new THREE.MeshBasicMaterial({color: 'green'})
-const capsule = new THREE.Mesh(geometry_capsule, material_capsule)
-scene.add(capsule)
-capsule.position.x = 0
-capsule.position.y = 10
-capsule.position.z = 10
+let earthMesh; // Store the reference to the loaded object
 
+loader.load( 'Earth.glb', function ( glb ) {
 
+    const root = glb.scene;
+    root.scale.set(0.03,0.03,0.03)
+
+    // Store the reference to the loaded object for rotation
+    earthMesh = glb.scene;
+
+	scene.add( glb.scene );
+    console.log(glb.scene)
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+// Lighting
+const ambientlight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientlight)
+
+const light = new THREE.DirectionalLight(0xffffff, 2)
+light.position.set(-20, 5, 30)
+scene.add(light)
+
+// const dlHelper = new THREE.DirectionalLightHelper(light, 25);
+// scene.add(dlHelper)
+
+const pointLight = new THREE.PointLight(0xedd59e, 15, 30)
+pointLight.position.set(-20, 5, 30)
+scene.add(pointLight)
+
+// const plHelper = new THREE.PointLightHelper(pointLight, 25)
+// scene.add(plHelper)
 
 const sizes = {
     width: 1000,
@@ -23,10 +49,10 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 30
 scene.add(camera)
-camera.position.z = 20
-camera.position.x = 0
-camera.position.y = 8
+
+
 
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
@@ -34,4 +60,24 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+
+let angle = 2.5; // Initial rotation angle
+let angle2 = -5.9;
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    // Update rotation angle
+    angle += 0.0009; // You can adjust the speed of rotation here
+    // Apply rotation to the object
+    if (earthMesh) {
+        earthMesh.rotation.y = angle;
+        earthMesh.rotation.x = angle2;
+
+    }
+
+    renderer.render(scene, camera)
+}
+
+
+animate()
